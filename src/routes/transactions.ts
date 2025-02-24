16093bd06d512cfc0825ead15029178d5c33bc88
Bruno <bruno.silva.castilho@gmail.com>
2025-02-24 18:33:39 -0300

    max age in seconds

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -45,7 +45,7 @@ export async function transactionsRoutes(app: FastifyInstance) {
       sessionId = randomUUID()
       reply.cookie('sessionId', sessionId, {
         path: '/',
-        maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+        maxAge: 60 * 60 * 24 * 7, // 7 days
       })
     }
 
